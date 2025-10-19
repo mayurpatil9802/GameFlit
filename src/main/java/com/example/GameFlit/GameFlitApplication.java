58392d50fe7ff725b393b0800c639644a4b2f53c
mayur <mp275483@gmail.com>
2025-10-19 11:07:01 +0530

    1:Start Project

---
+++ src/main/java/com/example/GameFlit/GameFlitApplication.java
@@ -0,0 +1,13 @@
+package com.example.GameFlit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameFlitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameFlitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GameFlit/GameFlitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GameFlit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameFlitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
